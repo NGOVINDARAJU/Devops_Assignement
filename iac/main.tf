e3ac46bb6ccb45a45016470e1a1271eb678bb4ea
NGOVINDARAJU <158595722+NGOVINDARAJU@users.noreply.github.com>
2025-09-15 23:59:57 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -86,4 +86,29 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 }
 
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket = aws_s3_bucket.app.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.app.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
 
